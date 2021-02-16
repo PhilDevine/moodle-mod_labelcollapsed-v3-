08e3f81c6c5a59d853397d0030b47a3eb38e0f82
PhilDevine <44176644+PhilDevine@users.noreply.github.com>
2021-02-16 09:22:18 +0000

    Update version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2012080801;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2020110900;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2010080300;  // Requires this Moodle version.
 $plugin->cron     = 0;           // Period for cron to check this module (secs).
 $plugin->component = 'mod_labelcollapsed';
