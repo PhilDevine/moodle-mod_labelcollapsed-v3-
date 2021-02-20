95eb9164e2c147e217487568549c7e406e7071e9
PhilDevine <44176644+PhilDevine@users.noreply.github.com>
2021-02-20 17:52:19 +0000

    Add files via upload

---
+++ backup/moodle2/backup_labelcollapsed_activity_task.class.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines backup_labelcollapsed_activity_task class
+ *
+ * @package     mod_labelcollapsed
+ * @category    backup
+ * @copyright   2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->dirroot . '/mod/labelcollapsed/backup/moodle2/backup_labelcollapsed_stepslib.php'); // Because it exists (must).
+
+/**
+ * Provides the steps to perform one complete backup of the Labelcollapsed instance
+ */
+class backup_labelcollapsed_activity_task extends backup_activity_task {
+
+    /**
+     * Define (add) particular settings this activity can have.
+     */
+    protected function define_my_settings() {
+        // No particular settings for this activity.
+    }
+
+    /**
+     * Define (add) particular steps this activity can have.
+     */
+    protected function define_my_steps() {
+        // Choice only has one structure step.
+        $this->add_step(new backup_labelcollapsed_activity_structure_step('labelcollapsed_structure', 'labelcollapsed.xml'));
+    }
+
+    /**
+     * No content encoding needed for this activity
+     *
+     * @param string $content some HTML text that eventually contains URLs to the activity instance scripts
+     * @return string the same content with no changes
+     */
+    static public function encode_content_links($content) {
+        return $content;
+    }
+}
